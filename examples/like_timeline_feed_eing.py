e6db2f5c7fd296b1933c8f4ff0bae11c0bbae074
WWZ <zeing@outlook.com>
2019-05-28 16:08:08 +0700

    update sh

--- examples/like_timeline_feed_eing.py
+++ examples/like_timeline_feed_eing.py
@@ -13,7 +13,7 @@ parser.add_argument('-u', type=str, help="username")
 parser.add_argument('-p', type=str, help="password")
 parser.add_argument('-proxy', type=str, help="proxy")
 args = parser.parse_args()
-
+print(args);
 bot = Bot()
 bot.login(username=args.u, password=args.p,
           proxy=args.proxy)
